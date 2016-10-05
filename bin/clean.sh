02033b4e8170fcbaf1c5c2dc8193b58b031201fd
Simon Parent <sboparen@gmail.com>
2016-10-04 23:17:06 -0400

    Script to update the pages branch with the built files.

---
+++ bin/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+rm -f *.pdf *.aux *.log *.out *.pdf.orig
+rm -f *.4ct *.4tc *.css *.dvi *.idv *.lg *.tmp *.xref
+rm -f resume.html partial.html
---
+++ bin/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Don't run unless we are clean.
+# See http://stackoverflow.com/questions/5139290
+if (git status --porcelain || echo err) | grep -q .; then exit 1; fi
+
+# Build.
+bin/clean.sh
+bin/build.sh
+
+# Copy files.
+cp page.html pages/index.html
+cp partial.html pages/_partial.html
+cp resume.pdf pages/simon-parent.pdf
+
+# Commit (if there were any changes).
+h="$(git describe --always)"
+cd pages
+if (git status --porcelain || echo err) | grep -q .; then
+    git commit -a -m "Build from commit $h."
+fi
