cce7c6b70341500b1728931fcd14172d304553bc
Simon Parent <sboparen@gmail.com>
2016-11-13 14:13:58 -0500

    Update.

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,8 @@ set -e
 pdflatex -halt-on-error -shell-escape resume
 bin/strip_pdf.sh resume.pdf
 htlatex resume "html,NoFonts,-css" '' '' "-halt-on-error"
-tidy -i -w 72 -b resume.html \
-    | awk '/<body>/{flag=1;next}/body>/{flag=0}flag' \
-    > partial.html
+sed -i resume.html -e 's/<!--[^>]*-->//g'
+tidy -m -i -w 72 -b resume.html
+L='<link rel="stylesheet" href="https://sboparen.github.io/style/main.css">'
+sed -i resume.html -e "s@</head>@$L</head>@"
+awk '/<body>/{flag=1;next}/body>/{flag=0}flag' resume.html >partial.html
